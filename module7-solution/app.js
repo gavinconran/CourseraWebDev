38da1f6bc6eebdb307c2db2f73a61c917dfb6d1c
gavin <gavinconran@yahoo.co.uk>
2017-01-22 19:31:22 +0000

    issue 10: error messages TODO

---
+++ module7-solution/app.js
@@ -0,0 +1,52 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var showToBuyList = this;
+
+  showToBuyList.items = ShoppingListCheckOffService.getToBuyItems();
+
+  showToBuyList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var showBoughtList = this;
+
+  showBoughtList.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var toBuyItems = [ { name: 'cookies', quantity: 10 },
+  					 { name: 'apples', quantity: 6 },
+  					 { name: 'cookies', quantity: 10 },
+  					 { name: 'oranges', quantity: 2 }];
+
+  var boughtItems = [  ];
+
+  service.removeItem = function(itemIdex) {
+  	var removedItem = toBuyItems.splice(itemIdex, 1);
+  	boughtItems.push(removedItem[0]);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
