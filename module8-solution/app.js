9574af82dffb94f045a3fa7a6c5340c5e3c690a3
gavin <gavinconran@yahoo.co.uk>
2017-01-26 15:56:53 +0000

    first draft - witkout directive

---
+++ module8-solution/app.js
@@ -0,0 +1,39 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menu = this;
+
+  var promise = MenuSearchService.getMatchedMenuItems();
+
+  promise.then(function (response) {
+    menu.found = response.data;
+  })
+  .catch(function (error) {
+    console.log("Something went terribly wrong.");
+  });
+
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    });
+
+    return response;
+  };
+
+}
+
+})();
